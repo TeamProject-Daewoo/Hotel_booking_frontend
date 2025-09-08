cc93d3b9ae602b7bdc418b038ee0b36a1b425bd6
Yongseok-2 <8465867@naver.com>
2025-09-08 21:03:46 +0900

    merge complete

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,16 @@
 // src/router/index.js
 import Main from '@/components/Main.vue'
 import SearchView from '@/components/SearchView.vue'
-import LoginView from '@/components/LoginView.vue'
-import RegisterView from '@/components/RegisterView.vue'
-import ProfileView from '@/components/ProfileView.vue'
+import LoginView from '@/views/LoginView.vue'
+import RegisterView from '@/views/RegisterView.vue'
+import ProfileView from '@/views/ProfileView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import MypageView from "@/views/MypageView.vue";
 
 const routes = [
   {path: "/", name: "main", component: Main},
   {path: "/search", name: "search", component: SearchView},
-  {path: "/mypage", name: "mypage", component: MypageView}
+  {path: "/mypage", name: "mypage", component: MypageView},
   {path: "/login", name: "login", component: LoginView},
   {path: "/register", name: "register", component: RegisterView},
   {path: "/profile", name: "profile", component: ProfileView}
