3a72b686ad9e9225ed10bd42395457cf220d48be
yongsung kim <kimys75128386@gmail.com>
2025-09-10 19:06:19 +0900

    axios 요청 테스트

--- src/components/search/InputForm.vue
+++ src/components/search/InputForm.vue
@@ -89,7 +89,11 @@ const tempGuestCount = ref(1);
 const emit = defineEmits(['search-trigger']);
 const search = () => {
     //백엔드 호출
-    //console.log(searchStore.getRequestPayload());
+    console.log(searchStore.getRequestPayload());
+    api.post('http://localhost:8888/api/search', searchStore.getRequestPayload())
+        .then(res => {
+            console.log(res);
+        })
     const response = [
         {'contentid':'143017', 'title':'가보호텔', 'image':'http://tong.visitkorea.or.kr/cms/resource/83/1942883_image2_1.jpg', 'price':'240000', 'address':'경기도 평택시 평택5로76번길 18-10', 'rating':'4.5', 'totalAminities':'20', 'totalReviews':'371'},
         {'contentid':'1865597', 'title':'가람초연재', 'image':'http://tong.visitkorea.or.kr/cms/resource/48/2993048_image2_1.jpg', 'price':'145000', 'address':'경상북도 안동시 풍천면 하회종가길 76-6', 'rating':'4.3', 'totalAminities':'10', 'totalReviews':'140'},
